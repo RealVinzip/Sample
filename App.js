aa112d59ea02d9e7f07bbb1e1bf2b9c342ae8253
Vincent Dieterich <v.m.d.op59@gmx.de>
2021-03-20 18:17:56 +0100

    Nothing

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+
+
+const itemView = (props) =>{
+  return (
+    <View style={styles.itemBox}> 
+      <Text>{props.item} {props.amount}</Text> 
+    </View>
+  );
+}
 
 export default function App() {
+  
+  const [items, setItem] = useState(
+    [{
+      item: "Item1",
+      amount: 1,
+      isSelected: false,
+    }]
+  )
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <NavigationContainer>
+      <View style={styles.container}>
+        <View>{items.map(item => <itemView item={item.item} amount={item.amount}/>)}</View>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer> 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemBox: {
+    backgroundColor: 'white',
+    flex: 1,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+  }
 });
